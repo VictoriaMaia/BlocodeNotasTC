7d38428735b03b61cef006c9aefe4eb1ed65492d
victoria_maia <victoria.maia@alu.ufc.br>
2020-01-31 09:07:37 -0300

    Update RunSuasNotasAppApplication.java

--- src/main/java/com/totalcross/RunSuasNotasAppApplication.java
+++ src/main/java/com/totalcross/RunSuasNotasAppApplication.java
@@ -4,6 +4,6 @@ import totalcross.TotalCrossApplication;
 
 public class RunSuasNotasAppApplication {
     public static void main(String [] args) {
-        TotalCrossApplication.run(SuasNotasApp.class, "/r", "5443444B3018A93884C5D622");
+        TotalCrossApplication.run(SuasNotasApp.class, "/r", "KEY");
     }
 }
